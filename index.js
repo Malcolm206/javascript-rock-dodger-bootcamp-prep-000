4d63b10892c22c5bac47babd0fd64cbd74764ef0
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2020-04-09 17:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -125,6 +125,7 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
+  ROCKS.forEach(function(rock) {rock.remove()})
   document.removeEventListener('keydown', moveDodger)
   alert("YOU LOSE!")
 }
